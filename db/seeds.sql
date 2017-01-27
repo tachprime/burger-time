3f58dffb13894cdacd5a05d54654800c524f1c1d
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-01-26 20:45:25 -0500

    Created DB with seed Data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,17 @@
 INSERT INTO `burgers_db`.`burgers` (
     `burger_name`,
-    `devoured`,
-    `date`,
-    );
-VALUES ('Cheeseburger', '0', '1/26/2017'), ('Hamburger', '0', '1/26/2017'), ('Veggie burger', '0', '1/26/2017');
+    `devoured`
+    )
+VALUES ('Cheeseburger', 0);
+
+INSERT INTO `burgers_db`.`burgers` (
+    `burger_name`,
+    `devoured`
+    )
+VALUES ('Hamburger', 0);
+
+INSERT INTO `burgers_db`.`burgers` (
+    `burger_name`,
+    `devoured`
+    )
+VALUES ('Veggie burger', 0);
