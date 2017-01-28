14751345c4f0dfc1148a3df6791f0e838668990d
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-01-27 23:07:28 -0500

    changes for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
-var password = require('./userpass'); //TODO replace with env var password @deploy
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: password.pass || '',
+    user: 'burgersAdmin',
+    port: 3306,
+    password: process.env.PASS || '',
     database: 'burgers_db'
 });
 
