900128d5346ff0e5a31bfb8ebede2a14ad88af47
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-01-28 20:21:05 -0500

    database settings changed

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host: 'us-cdbr-iron-east-04.cleardb.net',
-    user: 'b15af69be61557',
-    password: process.env.PASS || '',
-    database: 'heroku_1b8b9ba84effebe'
-});
+if(process.env.DATABASE_URL) {
+    connection = mysql.createConnection(process.env.DATABASE_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: process.env.PASS || '',
+        database: 'burgers_db'
+    });
+}
+
 
 module.exports = connection;
